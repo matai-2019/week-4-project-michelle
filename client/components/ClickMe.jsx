1dd7389fba53f7f7473794f931d8aad2c55096d1
EDA <eteroalafaele080@gmail.com>
2019-06-21 15:06:29 +1200

    fjdjds

--- client/components/ClickMe.jsx
+++ client/components/ClickMe.jsx
@@ -5,14 +5,22 @@ export default class ClickMe extends React.Component {
     super(props)
     this.state = { fade: false }
   }
+
+  getCoordinates = () => {
+    const randX = Math.floor(Math.random() * (window.innerWidth - 100));
+    const randY = Math.floor(Math.random() * (window.innerHeight - 100));
+    return { buttonstyle:{top: randX, left: randY} }
+  }
+
   render() {
     const fade = this.state.fade
 
     return (
       <button
+        style={this.state.buttonstyle}
         ref='button'
         onClick={() => this.setState({ fade: true })}
-        onAnimationEnd={() => setTimeout(this.setState({ fade: false }), 5000)}
+        onAnimationEnd={() => setTimeout(this.setState({ fade: false, ...getCoordinates() }), 5000)}
         className={fade ? 'fade' : ''}>
         Click me!
       </button>
--- server/public/bundle.js
+++ server/public/bundle.js
@@ -218,6 +218,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
 function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
@@ -226,13 +228,15 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
 
 function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
 
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
 function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
 
 function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 
 
@@ -247,6 +251,18 @@ function (_React$Component) {
     _classCallCheck(this, ClickMe);
 
     _this = _possibleConstructorReturn(this, _getPrototypeOf(ClickMe).call(this, props));
+
+    _defineProperty(_assertThisInitialized(_this), "getCoordinates", function () {
+      var randX = Math.floor(Math.random() * (window.innerWidth - 100));
+      var randY = Math.floor(Math.random() * (window.innerHeight - 100));
+      return {
+        buttonstyle: {
+          top: randX,
+          left: randY
+        }
+      };
+    });
+
     _this.state = {
       fade: false
     };
@@ -260,6 +276,7 @@ function (_React$Component) {
 
       var fade = this.state.fade;
       return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
+        style: this.state.buttonstyle,
         ref: "button",
         onClick: function onClick() {
           return _this2.setState({
@@ -267,9 +284,9 @@ function (_React$Component) {
           });
         },
         onAnimationEnd: function onAnimationEnd() {
-          return setTimeout(_this2.setState({
+          return setTimeout(_this2.setState(_objectSpread({
             fade: false
-          }), 5000);
+          }, getCoordinates())), 5000);
         },
         className: fade ? 'fade' : ''
       }, "Click me!");
