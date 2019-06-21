f09ca6fc1fc0c39d4a2367850501d48e0bb55127
Joanna Klimas <klimas.j.m@gmail.com>
2019-06-21 12:08:26 +1200

    two tests

--- tests/app.test.js
+++ tests/app.test.js
@@ -2,9 +2,27 @@ import React from 'react'
 import { mount } from 'enzyme'
 
 import App from '../client/components/App'
+import Home from '../client/components/Home'
+import Question from '../client/components/Question'
 
 test('<App />', () => {
-  const expected = 'React development has begun!'
+  const expected = 'Anti-personality Test'
   const wrapper = mount(<App />)
   expect(wrapper.text()).toMatch(expected)
+})
+
+test('<App /> contains <Home />', () => {
+  const expected = true
+  const wrapper = mount(<App />)
+  const actual = wrapper.containsMatchingElement(Home)
+
+  expect(actual).toBe(expected)
+})
+
+test('<App /> contains <Question />', () => {
+  const expected = true
+  const wrapper = mount(<App />)
+  const actual = wrapper.containsMatchingElement(Question)
+
+  expect(actual).toBe(expected)
 })
